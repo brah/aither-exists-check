200ab9d1c0fe18ddae2ad8f5f131a0035693afad
Dylan <24949824+brah@users.noreply.github.com>
2023-04-03 11:35:03 +1000

    first commit

---
+++ apiKeySample.py
@@ -0,0 +1,3 @@
+# create apiKey.py and mimic this file
+aither_key = "AITHER_KEY_HERE" # https://aither.cc/users/YOUR_USERNAME/settings/security
+radarr_key = "RADARR_KEY_HERE" # radarr -> settings -> general
---
+++ main.py
@@ -0,0 +1,50 @@
+import apiKey
+import requests
+import time
+
+radarr_url = "http://media.server:7878/api/v3/movie"
+headers = {"X-Api-Key": apiKey.radarr_key}
+
+response = requests.get(radarr_url, headers=headers)
+
+aither_url = "https://aither.cc"
+
+# Function to get all movies from Radarr
+def get_all_movies():
+    response = requests.get(f"{radarr_url}", headers=headers)
+    movies = response.json()
+    return movies
+
+
+# Function to search for a movie in UNIT3D using its TMDB ID
+def search_movie(tmdb_id):
+    response = requests.get(
+        f"{aither_url}/api/torrents/filter?tmdbId={tmdb_id}&api_token={apiKey.aither_key}"
+    )
+    torrents = response.json()["data"]
+    return torrents
+
+
+# Get all movies from Radarr
+movies = get_all_movies()
+
+# Open a file for writing "Not found" messages
+not_found_file = open("not_found.txt", "w")
+
+# Loop through each movie and search for it in UNIT3D using its TMDB ID
+for movie in movies:
+    title = movie["title"]
+    tmdb_id = movie["tmdbId"]
+    print(f"Checking {title}... ", end="")
+    torrents = search_movie(tmdb_id)
+    if len(torrents) == 0:
+        print("Not found in UNIT3D")
+        # Write the "Not found" message to the file
+        not_found_file.write(f"{title} not found in AITHER\n")
+        time.sleep(0.5)
+    else:
+        print("Found in AITHER")
+        time.sleep(0.5)
+
+# Close the file
+not_found_file.close()
