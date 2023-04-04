9cc2410e3700a715624f346d39653747250548d9
Dylan <24949824+brah@users.noreply.github.com>
2023-04-04 11:40:27 +1000

    add sonarr/shows check

--- apiKeySample.py
+++ apiKeySample.py
@@ -1,3 +1,4 @@
 # create apiKey.py and mimic this file
 aither_key = "AITHER_KEY_HERE" # https://aither.cc/users/YOUR_USERNAME/settings/security
 radarr_key = "RADARR_KEY_HERE" # radarr -> settings -> general
+sonarr_key = "SONARR_KEY_HERE" # sonarr -> settings -> general
--- mainsonarr.py
+++ mainsonarr.py
@@ -2,39 +2,39 @@ import apiKey
 import requests
 import time
 
-radarr_url = "http://RADARR_URL:RADARR_PORT/api/v3/movie"
-headers = {"X-Api-Key": apiKey.radarr_key}
+sonarr_url = "http://SONARR_URL:SONARR_PORT/api/series"
+headers = {"X-Api-Key": apiKey.sonarr_key}
 
 aither_url = "https://aither.cc"
 
 # Function to get all movies from Radarr
-def get_all_movies():
-    response = requests.get(f"{radarr_url}", headers=headers)
+def get_all_shows():
+    response = requests.get(f"{sonarr_url}", headers=headers)
     movies = response.json()
     return movies
 
 
 # Function to search for a movie in UNIT3D using its TMDB ID
-def search_movie(tmdb_id):
+def search_show(tvdb_id):
     response = requests.get(
-        f"{aither_url}/api/torrents/filter?tmdbId={tmdb_id}&api_token={apiKey.aither_key}"
+        f"{aither_url}/api/torrents/filter?tvdbId={tvdb_id}&api_token={apiKey.aither_key}"
     )
     torrents = response.json()["data"]
     return torrents
 
 
 # Get all movies from Radarr
-movies = get_all_movies()
+shows = get_all_shows()
 
 # Open a file for writing "Not found" messages
-not_found_file = open("not_found.txt", "w")
+not_found_file = open("not_found_sonarr.txt", "w")
 
 # Loop through each movie and search for it in UNIT3D using its TMDB ID
-for movie in movies:
-    title = movie["title"]
-    tmdb_id = movie["tmdbId"]
+for show in shows:
+    title = show["title"]
+    tvdb_id = show["tvdbId"]
     print(f"Checking {title}... ", end="")
-    torrents = search_movie(tmdb_id)
+    torrents = search_show(tvdb_id=tvdb_id)
     if len(torrents) == 0:
         print("Not found in Aither")
         # Write the "Not found" message to the file
